c9be77f6512087deedb890abcc78b16782d25a95
Rijan77 <rijanacharya73@gmail.com>
2025-12-27 19:37:12 +0545

    Setup clean architecture for application

---
+++ lib/app/feature/homepage/presentation/widgets/home_screen_widget/top_header.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class TopHeader extends StatefulWidget {
+  const TopHeader({super.key});
+
+  @override
+  State<TopHeader> createState() => _TopHeaderState();
+}
+
+class _TopHeaderState extends State<TopHeader> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
